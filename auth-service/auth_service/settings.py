26864600e712e8ddfa8035a921f838106dfe5c58
DavidK1412 <davidcasallas013@gmail.com>
2025-02-19 07:38:57 -0500

    test

--- auth-service/auth_service/settings.py
+++ auth-service/auth_service/settings.py
@@ -112,14 +112,7 @@ print('--------------------------')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),  # Valor predeterminado si no se encuentra
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': os.getenv('DATABASE_URL')
 }
 
 
