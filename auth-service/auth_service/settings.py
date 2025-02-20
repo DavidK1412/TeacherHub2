0ac5d632867e46eee3df909ec6e62749283041a3
DavidK1412 <davidcasallas013@gmail.com>
2025-02-20 09:28:50 -0500

    wsgi

--- auth-service/auth_service/settings.py
+++ auth-service/auth_service/settings.py
@@ -151,7 +151,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'auth_service/static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
